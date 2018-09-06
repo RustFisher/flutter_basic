8bd9dbce929f540b018db832e9003ae656944f77
RustFisher <rustfisher@foxmail.com>
2018-09-06 09:22:25 +0800

    MODIFY handling_gestures
    ADD swipe_to_dismiss_page

--- lib/samples/gestures/handling_gestures.dart
+++ lib/samples/gestures/handling_gestures.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/samples/gestures/swipe_to_dismiss_page.dart';
 
 /// 手势检测示例界面
 class HandlingGesturesPage extends StatelessWidget {
@@ -9,7 +10,14 @@ class HandlingGesturesPage extends StatelessWidget {
       appBar: AppBar(title: Text('Handling gestures'),),
       body: Center(
         child: Column(children: <Widget>[
-          new ButtonSample1(),
+          new ButtonSample1("Swipe to dismiss demo"),
+          new ButtonSample2(),
+          new RaisedButton(onPressed: () {
+            print("click $this");
+          }, child: Text("RaisedButton"),),
+          new FlatButton(onPressed: () {
+            print("click $this");
+          }, child: Text("FlatButton"),),
         ],),
 
       ),);
@@ -18,12 +26,18 @@ class HandlingGesturesPage extends StatelessWidget {
 
 /// 自定义button
 class ButtonSample1 extends StatelessWidget {
+  var _text = "";
+
+  ButtonSample1(text) {
+    _text = text;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        final snackBar = SnackBar(content: Text("Now you tap the $this"),);
-        Scaffold.of(context).showSnackBar(snackBar);
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => (SwipeToDismissPage())));
       },
       child: Container(
         padding: EdgeInsets.all(12.0),
@@ -32,8 +46,25 @@ class ButtonSample1 extends StatelessWidget {
             .of(context)
             .buttonColor,
             borderRadius: BorderRadius.circular(8.0)),
-        child: Text("$this"),),
+        child: Text(_text),),
     );
   }
 
+}
+
+/// 带有波纹效果的按钮
+class ButtonSample2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        final snackBar = SnackBar(content: Text("Now you tap the $this"),);
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        child: Text("$this"),
+      ),
+    );
+  }
 }
---
+++ lib/samples/gestures/swipe_to_dismiss_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+/// 侧滑删除列表界面
+class SwipeToDismissPage extends StatefulWidget {
+  SwipeToDismissPage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SwipeState();
+  }
+}
+
+class SwipeState extends State<SwipeToDismissPage> {
+  final items = List<String>.generate(10, (i) => "Item $i"); // 列表必须放在外面定义
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Swipe to dismiss",
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Dismissible(
+              key: Key(item),
+              onDismissed: (direction) {
+                setState(() {
+                  items.removeAt(index);
+                  print(direction);
+                });
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('remove $item')));
+              },
+              child: ListTile(
+                title: Text('$item'),
+              ),
+              background: Container(
+                color: Colors.red,
+              ),
+            );
+          }),
+    );
+  }
+}
